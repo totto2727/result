929859765608e02297ab7a1d10129329a5a9f9b3
totto2727 <h_tsuchida@yumemi.co.jp>
2023-11-30 02:56:46 +0900

    feat: optimize build

--- build.ts
+++ build.ts
@@ -1,3 +1,5 @@
+import * as esbuild from "esbuild";
+
 const targetBasePath = `${import.meta.dir ?? "."}/src`;
 const distBasePath = `${import.meta.dir ?? "."}/dist`;
 const entrypoints = [
@@ -6,22 +8,29 @@ const entrypoints = [
   `${targetBasePath}/lazy.ts`,
 ];
 
-await Bun.build({
-  entrypoints,
-  outdir: `${distBasePath}/browser`,
-  target: "browser",
-  format: "esm",
-  splitting: true,
-  minify: true,
-  naming: "[dir]/[name].[ext]",
-});
-
-await Bun.build({
-  entrypoints,
-  outdir: `${distBasePath}/import`,
-  target: "node",
-  format: "esm",
-  splitting: true,
-  minify: true,
-  naming: "[dir]/[name].m[ext]",
-});
+await Promise.all([
+  Bun.build({
+    entrypoints,
+    outdir: `${distBasePath}/import`,
+    target: "node",
+    format: "esm",
+    // splitting: true,
+    minify: true,
+    naming: "[dir]/[name].m[ext]",
+  })
+    .then(console.info)
+    .catch(console.error),
+  esbuild
+    .build({
+      entryPoints: entrypoints,
+      bundle: true,
+      minify: true,
+      outdir: `${distBasePath}/require`,
+      platform: "node",
+      format: "cjs",
+      target: "esnext",
+      outExtension: { ".js": ".cjs" },
+    })
+    .then(console.info)
+    .catch(console.error),
+]);
