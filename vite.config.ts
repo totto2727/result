b732519027479e139b92508fd3fe9ca5c717ca31
totto2727 <h_tsuchida@yumemi.co.jp>
2023-11-29 17:48:05 +0900

    refactor: change scripts for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
+const targetBasePath = `${import.meta.dir ?? "."}/src`;
+const distBasePath = `${import.meta.dir ?? "."}/dist`;
+
 export default defineConfig({
   build: {
-    outDir: "./dist/require",
+    outDir: `${distBasePath}/require`,
     minify: "esbuild",
     lib: {
-      entry: [resolve(__dirname, "index.ts")],
-      fileName: "index",
+      entry: [resolve(targetBasePath, "index.ts")],
       formats: ["cjs"],
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].cjs",
+      },
     },
   },
 });
