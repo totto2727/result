cfaaa90e1d01c9fd95cf319fae9c0fbb07a97153
totto2727 <h_tsuchida@yumemi.co.jp>
2023-11-29 23:07:40 +0900

    feat: add build settings

--- build.ts
+++ build.ts
@@ -1,8 +1,13 @@
 const targetBasePath = `${import.meta.dir ?? "."}/src`;
 const distBasePath = `${import.meta.dir ?? "."}/dist`;
+const entrypoints = [
+  `${targetBasePath}/index.ts`,
+  `${targetBasePath}/eager.ts`,
+  `${targetBasePath}/lazy.ts`,
+];
 
 await Bun.build({
-  entrypoints: [`${targetBasePath}/index.ts`],
+  entrypoints,
   outdir: `${distBasePath}/browser`,
   target: "browser",
   format: "esm",
@@ -12,7 +17,7 @@ await Bun.build({
 });
 
 await Bun.build({
-  entrypoints: [`${targetBasePath}/index.ts`],
+  entrypoints,
   outdir: `${distBasePath}/import`,
   target: "node",
   format: "esm",
